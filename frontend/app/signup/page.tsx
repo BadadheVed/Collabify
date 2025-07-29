3b56d322231fcea6c8d213b9f32589283d1d1d72
Ved Badadhe <vbbadadhe@gmail.com>
2025-07-29 18:01:27 +0530

    Removed Minor Bugs

--- frontend/app/signup/page.tsx
+++ frontend/app/signup/page.tsx
@@ -273,15 +273,15 @@ export default function SignupPage() {
               </Button>
             </form>
 
-            {/* Divider */}
+            {/* Divider
             <div className="my-6 flex items-center">
               <div className="flex-1 border-t border-gray-600"></div>
               <span className="px-4 text-gray-400 text-sm">or</span>
               <div className="flex-1 border-t border-gray-600"></div>
-            </div>
+            </div> */}
 
             {/* Social Signup */}
-            <div className="space-y-3">
+            {/* <div className="space-y-3">
               <Button
                 type="button"
                 variant="outline"
@@ -309,7 +309,7 @@ export default function SignupPage() {
                 Continue with Google
               </Button>
 
-              <Button
+              {/* <Button
                 type="button"
                 variant="outline"
                 disabled={isLoading}
@@ -318,13 +318,11 @@ export default function SignupPage() {
                 <svg
                   className="w-5 h-5 mr-2"
                   fill="currentColor"
-                  viewBox="0 0 24 24"
                 >
                   <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z" />
                 </svg>
                 Continue with Facebook
-              </Button>
-            </div>
+              </Button> */}
 
             {/* Login Link */}
             <p className="text-center text-gray-400 mt-8">
